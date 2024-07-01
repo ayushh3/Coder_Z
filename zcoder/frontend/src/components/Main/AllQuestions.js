7c609e8e2f306ac4f9891ed034691f83cfb52fe2
abhinormal <122599194+abhinormal@users.noreply.github.com>
2024-07-01 23:25:38 +0530

    Updated AllQuestions.js

--- zcoder/frontend/src/components/Main/AllQuestions.js
+++ zcoder/frontend/src/components/Main/AllQuestions.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './allquestions.css';
 import ReactHtmlParser from 'html-react-parser'
-
+//ok
 function AllQuestions({ question }) {
 
     const tags = JSON.parse(question?.tags[0]);
